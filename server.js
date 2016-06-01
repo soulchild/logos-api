29a6a632118abd176e2343501c2b2b75047621ba
Tobias Kremer <tobias.kremer@gmail.com>
2016-06-01 16:43:54 +0200

    Use Express.
    Updated logos.

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 "use strict";
 
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 const path = require('path');
-const SVGPORN = path.join(process.cwd(), 'logos', 'svgporn');
-const svgPornLogos = require(path.join(SVGPORN, 'app', '/logos.json')).items;
 
 const PORT = process.env.PORT || 8000;
 
-function getLogos(query) {
+const PATH_SVGPORN = path.join(process.cwd(), 'logos', 'svgporn');
+const PATH_LOGOS = path.join(PATH_SVGPORN, 'logos');
+
+const svgPornLogos = require(path.join(PATH_SVGPORN, 'app', '/logos.json')).items;
+
+function searchLogos(query, key) {
   if (!query) return [];
+  key = key || 'shortname';
   let queryClean = query.replace(/[.\-() ]/gi, '').toLowerCase();
   return svgPornLogos.filter(logo => {
-    return logo.shortname.indexOf(queryClean) > -1;
+    return logo[key].indexOf(queryClean) > -1;
   });
 }
 
@@ -23,59 +26,47 @@ function getLogo(key, value) {
   });
 }
 
-function serveJSON(res, result) {
+/* Global route */
+app.get('/*', (req,res,next) => {
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(result));
-}
-
-function handleRequest(req, res) {
-  let parsedURL = url.parse(req.url, true);
-  let params = parsedURL.query;
-  let urlPath = parsedURL.path;
-  let query = params.query;
-  let result;
-
-  if (query) {
-    // Search logos
-    serveJSON(res, getLogos(query));
-    return;
-  } else if (urlPath) {
-    // Serve logo
-    const params = urlPath.match(/^\/(.+)$/);
-    if (params) {
-      const logoId = params[1];
-      const logo = getLogo('shortname', logoId);
-      if (logo) {
-        const logoPath = path.join(SVGPORN, 'logos');
-        const logoFilename = logo.files[0];
-        const logoFile = path.join(logoPath, logoFilename);
-        fs.stat(logoFile, (err, stats) => {
-          if (err || !stats || !stats.isFile()) return;
-          fs.readFile(logoFile, 'binary', (err, file) => {
-            res.setHeader('Content-Type', 'image/svg+xml');
-            res.write(file, 'binary');
-            res.end();
-          });
-        });
-        return;
-      }
-    }
-  }
-
-  if (!result) {
-    res.statusCode = 404;
-    result = {
-      error: 'Not found'
-    };
-    serveJSON(res, result);
-  }
-}
-
-const server = http.createServer(handleRequest);
-server.on('clientError', (err, socket) => {
-  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+  next();
 });
 
-server.listen(PORT, () => {
+/* Search logos */
+app.get('/?', (req, res) => {
+  let logos = searchLogos(req.query.query);
+  res.json(logos);
+});
+
+/* Get logo */
+app.get('/:logo', (req, res, next) => {
+  const logo = getLogo('shortname', req.params.logo);
+  if (logo) {
+    const logoFilename = logo.files[0];
+    const logoFile = path.join(PATH_LOGOS, logoFilename);
+    res.sendFile(logoFile, {
+      headers: {
+        'Content-Type': 'image/svg+xml'
+      }
+    });
+    return;
+  }
+  next();
+});
+
+/* 404 */
+app.all('*', (req, res) => {
+  res.status(404).json({
+    error: 'Not found'
+  });
+});
+
+/* Errors */
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(err.status || 500).json(err.message);
+});
+
+app.listen(PORT, () => {
   console.log("Logos API listening on port %d.", PORT);
 });
