0b6dec1a6d0ae1073f4c873887690a7e38ea1b81
Tobias Kremer <tobias.kremer@gmail.com>
2016-11-30 16:22:17 +0100

    Made logo loading async to allow loading logos via network.
    Use items.json from firebase for gilbarbara's logo collection because
    the app-branch is no longer maintained (see
    https://github.com/gilbarbara/logos/issues/195)
    
    Added eslintrc and pre-commit linting hook.

--- server.js
+++ server.js
@@ -1,59 +1,60 @@
-"use strict";
+'use strict';
 
 const path = require('path');
 const express = require('express');
+const logos = require('./lib/logos');
 const app = express();
 
 const PORT = process.env.LOGOSAPI_PORT || 8000;
 const BASE_URL = process.env.LOGOSAPI_URL || 'http://localhost:' + PORT;
 const LOGOS_BASE_PATH = path.join(process.cwd(), 'logos');
 
-const logos = require('./lib/logos')(LOGOS_BASE_PATH, {
+logos.init(LOGOS_BASE_PATH, {
   baseURL: BASE_URL
-});
-
-/* Search logos */
-app.get('/?', (req, res) => {
-  const { q: shortname, source } = req.query;
-  let conditions = Object.assign({},
-    shortname ? { shortname } : {},
-    source    ? { source } : {}
-  );
-  res.json(logos.search(conditions));
-});
-
-app.get('/ping', (req, res) => {
-  res.send('pong');
-});
-
-/* Serve logo image */
-app.get('/:id', (req, res, next) => {
-  const logo = logos.findById(req.params.id);
-  if (logo) {
-    const logoFile = path.join(LOGOS_BASE_PATH, logo.path);
-    res.sendFile(logoFile, {
-      headers: {
-        'Content-Type': 'image/svg+xml'
-      }
-    });
-    return;
-  }
-  next();
-});
-
-/* 404 */
-app.all('*', (req, res) => {
-  res.status(404).json({
-    error: 'Not found'
+}).then(logosAPI => {
+  /* Search logos */
+  app.get('/?', (req, res) => {
+    const { q: shortname, source } = req.query;
+    let conditions = Object.assign({},
+      shortname ? { shortname } : {},
+      source    ? { source } : {}
+    );
+    res.json(logosAPI.search(conditions));
   });
-});
 
-/* Errors */
-app.use(function(err, req, res) {
-  console.error(err.stack);
-  res.status(err.status || 500).json(err.message);
-});
+  app.get('/ping', (req, res) => {
+    res.send('pong');
+  });
 
-app.listen(PORT, () => {
-  console.log("Logos API serving %d logos on port %d.", logos.all.length, PORT);
+  /* Serve logo image */
+  app.get('/:id', (req, res, next) => {
+    const logo = logosAPI.findById(req.params.id);
+    if (logo) {
+      const logoFile = path.join(LOGOS_BASE_PATH, logo.path);
+      res.sendFile(logoFile, {
+        headers: {
+          'Content-Type': 'image/svg+xml'
+        }
+      });
+      return;
+    }
+    next();
+  });
+
+  /* 404 */
+  app.all('*', (req, res) => {
+    res.status(404).json({
+      error: 'Not found'
+    });
+  });
+
+  /* Errors */
+  app.use(function(err, req, res) {
+    console.error(err.stack);
+    res.status(err.status || 500).json(err.message);
+  });
+
+  app.listen(PORT, () => {
+    console.log('Logos API serving %d logos on port %d.', logosAPI.logos.length, PORT);
+  });
 });
