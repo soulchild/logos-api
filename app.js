5afa485157b403688c1f37ab3aeba7273aa0a32f
Tobias Kremer <tobias.kremer@dg-i.net>
2024-09-26 09:05:59 +0200

    chore: release v0.4.1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const path = require('path');
+const path = require('node:path');
 const express = require('express');
 const logger = require('./lib/logger');
 const logos = require('./lib/logos');
